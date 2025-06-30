901a5b6aead04d830e9a16dbe049fbd773dde477
Ferraz <warley.ferraz.wf@gmail.com>
2025-06-30 00:05:55 -0300

    adicionando migrations antigas e corrigindo-as

---
+++ migrations/versions/04fdeac0e70f_merge_branches.py
@@ -0,0 +1,26 @@
+# /migrations/versions/04fdeac0e70f_merge_branches.py
+
+"""Merge branches
+
+Revision ID: 04fdeac0e70f
+Revises: 4b91b8f80e08, f0898c3bfea6
+Create Date: 2025-06-29 23:51:14.149569
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '04fdeac0e70f'
+down_revision = ('4b91b8f80e08', 'f0898c3bfea6')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
