ee3f3e8ac29c29f3d60890dd6f5f9bf047aec985
Ferraz <warley.ferraz.wf@gmail.com>
2025-07-15 01:33:18 -0300

    merge heads para corrigir múltiplos heads

---
+++ migrations/versions/125c7b02ed09_merge_heads.py
@@ -0,0 +1,22 @@
+"""merge heads
+
+Revision ID: 125c7b02ed09
+Revises: 
+Create Date: 2025-07-15 01:31:51.820162
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '125c7b02ed09'
+down_revision = ('03566aa0d327', '4db4f85650c4')
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
+    pass
